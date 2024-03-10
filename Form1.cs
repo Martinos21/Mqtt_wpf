3124c99f131a8bfa82ac3fc3a1991c4dc990a530
Martehn03 <encantr@seznam.cz>
2024-03-10 19:27:49 +0100

    multiple topics sub

--- Form1.cs
+++ Form1.cs
@@ -30,7 +30,16 @@ namespace Mqtt_homeapp
         private void MqClient_MqttMsgPublishReceived(object sender, MqttMsgPublishEventArgs e)
         {
             var message = Encoding.UTF8.GetString(e.Message);
-            label2.Invoke((MethodInvoker)(() => label2.Text = message));
+            //label2.Invoke((MethodInvoker)(() => label2.Text = message));
+
+            if (e.Topic == "test1892")
+            {
+                label2.Invoke((MethodInvoker)(() => label2.Text = message));
+            }
+            else if (e.Topic == "test1893")
+            {
+                label4.Invoke((MethodInvoker)(() => label4.Text = message));
+            }
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -39,7 +48,7 @@ namespace Mqtt_homeapp
             {
                 mqttClient = new MqttClient("broker.hivemq.com");
                 mqttClient.MqttMsgPublishReceived += MqClient_MqttMsgPublishReceived;
-                mqttClient.Subscribe(new string[] {"test1892"}, new byte[] {MqttMsgBase.QOS_LEVEL_AT_LEAST_ONCE});
+                mqttClient.Subscribe(new string[] {"test1892", "test1893"}, new byte[] {MqttMsgBase.QOS_LEVEL_AT_LEAST_ONCE, MqttMsgBase.QOS_LEVEL_AT_LEAST_ONCE});
                 mqttClient.Connect("testik");
             });
         }
